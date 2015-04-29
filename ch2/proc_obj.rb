cccc4c1ce9a756c31b482ade6eceee5a95ab55c4
Yu Hoshino <yhoshino11@gmail.com>
2015-04-30 00:03:51 +0900

    Proc with args

---
+++ ch2/proc_obj.rb
@@ -0,0 +1,22 @@
+# stdout to variable
+# ex.) res = capture_stdout { puts 'hello world' }
+def capture_stdout
+  out = StringIO.new
+  $stdout = out
+  yield
+  return out.string
+ensure
+  $stdout = STDOUT
+end
+
+require 'rspec'
+
+RSpec.describe 'Proc' do
+  it 'with args' do
+    greeter = proc do |name|
+      puts "Hello, #{name}!"
+    end
+
+    expect(capture_stdout { greeter.call('Proc') }).to eq("Hello, Proc!\n")
+  end
+end
